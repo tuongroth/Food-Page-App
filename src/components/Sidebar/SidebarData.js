2bf04afad27a2220269a9c74cf7ab543881286c0
tuongroth <44430600+tuongroth@users.noreply.github.com>
2025-02-26 19:14:23 +0200

    Create SidebarData.js

---
+++ src/components/Sidebar/SidebarData.js
@@ -0,0 +1,8 @@
+const SidebarData = [
+  { title: "🏠 Home", path: "/" },
+  { title: "🛒 Cart", path: "/cart" },
+  { title: "📦 Orders", path: "/orders" },
+  { title: "⚙️ Settings", path: "/settings" },
+];
+
+export default SidebarData;
